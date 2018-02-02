a307811e17a9c938aeeaf6cbdd32e20cf4425262
Unknown <stoudtlr@hotmail.com>
2018-02-01 19:45:06 -0800

    added stuff
    added stuff

--- TestScript.ps1
+++ TestScript.ps1
@@ -1,3 +1,19 @@
-#Region Readme
-Get-Process
-#EndRegion Readme
+#region Readme
+This is a test script just to play with git and gitKraken
+#endregion
+
+#region CrashPC
+Start-Process  -FilePath C:\WINDOWS\system32\notepad.exe
+$Processes = $(Get-Process).Name
+while ($Processes -match "notepad") {
+    Start-Process  -FilePath C:\WINDOWS\system32\notepad.exe
+}    
+#endregion
+
+#region SlowPC
+$times = 1..100
+foreach ($time in $times) {
+    Start-Sleep -Seconds 1
+    Start-Process  -FilePath C:\WINDOWS\system32\notepad.exe
+}
+#endregion
